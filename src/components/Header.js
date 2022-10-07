dbe06dffe206aa66924d1bc5f374c12f987f5938
RkKhoaNNV <khoannv@rikkeisoft.com>
2022-10-07 09:11:09 +0700

    fix title
    Signed-off-by: RkKhoaNNV <khoannv@rikkeisoft.com>

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ function Header() {
   return (
     <header>
       <video src="/video.mp4" loop autoPlay muted></video>
-      <h1>Beautiful House</h1>
+      <h1>Chuột đụ</h1>
       <div className="row">
         {/* <button className="btn" style={{ cursor: "pointer" }}>
           Sign Up
